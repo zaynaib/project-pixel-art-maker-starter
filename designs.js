5c68c2476093567e721e71fff2e2aac0f708ce09
Zaynaib Giwa <zaynaibg@gmail.com>
2018-03-17 23:07:15 -0500

    grid works. work on color function

--- designs.js
+++ designs.js
@@ -1,10 +1,56 @@
-// Select color input
-// Select size input
+//variable
+//resources
+//https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/color
 
-// When size is submitted by the user, call makeGrid()
+$(document).ready(function() {
+	var column;
+	var row;
+	var color;
 
-function makeGrid() {
+	//black is default color
+	color = $('input[type=color]');
+	var bgColor = color.val();
+	console.log(bgColor);
 
-// Your code goes here!
+	//on change change color of color picker
+	color.on('change', function() {
+		bgColor = color.val();
+	});
 
-}
+	console.log(bgColor);
+
+	$('input[type=submit]').on('click', function(event) {
+		event.preventDefault();
+
+		row = $('#inputHeight').val();
+		column = $('#inputWeight').val();
+		//console.log(row)
+		//console.log(column)
+		for (var i = 0; i < row; i++) {
+			$('#pixelCanvas').append('<tr></tr>');
+			//console.log("row created!")
+
+			for (var j = 0; j < column; j++) {
+				$('#pixelCanvas')
+					.children()
+					.last()
+					.append('<td></td>');
+
+				//console.log("column created")
+			} //end for loop for column
+		} //for loop for rows
+	}); // end of funtion
+
+	//change the color of the cell
+	$('#pixelCanvas').on('click', 'td', function() {
+		//$(this).css({"background-color":"red"})
+		$(this).css({ 'background-color': 'red' });
+		console.log('Its been clicked');
+	});
+
+	//$("ul").find("span").css({"color": "red", "border": "2px solid red"});
+
+	function makeGrid() {
+		// Your code goes here!
+	}
+});
